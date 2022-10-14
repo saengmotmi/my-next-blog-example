1dc9701a0409888ff1172832af89fdddfd36816e
saengmotmi <ohjtack@gmail.com>
2022-10-14 22:10:04 +0900

    swr

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-export const nextConfig = {
+const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
 };
+
+module.exports = nextConfig;
